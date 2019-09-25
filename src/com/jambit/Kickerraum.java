f2ca34e2ff82658b3e4ee391310da5f10edd8073
ceisenschmid <cosima.eisenschmid@jambit.com>
2019-09-25 11:32:16 +0200

    man kann jetzt kickern

--- src/com/jambit/Kickerraum.java
+++ src/com/jambit/Kickerraum.java
@@ -1,7 +1,14 @@
 package com.jambit;
 
+import java.util.Scanner;
+
 public class Kickerraum extends Room {
-    int position=202;
+    int position = 202;
+    int zufallszahl;
+    int match;
+    int toreplayer = 0;
+    int torematch = 0;
+    public static Scanner kicker = new Scanner(System.in);
 
     void execute(Player player) {
         raumName = "Kickerraum";
@@ -9,6 +16,7 @@ public class Kickerraum extends Room {
         option(player, position);
         switch (auswahl) {
             case 3:
+                kickern();
                 break;
             case 4:
                 player.position = 5;
@@ -16,7 +24,31 @@ public class Kickerraum extends Room {
         }
     }
 
-    void setKickerraum(){
+    void kickern() {
+        while (torematch != 5 && toreplayer != 5) {
+            zufallszahl = (int) (Math.random() * 20) + 1;
+            System.out.println(zufallszahl); // TODO muss wieder entfernt werden
+            System.out.println("Gib eine Zahl zwischen 1 und 20 ein.");
+            match = kicker.nextInt();
+            if (zufallszahl - match > 5 || zufallszahl - match < -5) {
+                torematch++;
+                System.out.println("Du hast ein Tor kassiert, es steht " + toreplayer + ":" + torematch + ", für dich.");
+            } else {
+                toreplayer++;
+                System.out.println("Super! Du hast ein Tor geschossen, es steht " + toreplayer + ":" + torematch + ", für dich.");
+            }
+        }
+        if (toreplayer == 5){
+            System.out.println("Glückwunsch, du hast gewonnen.");
+            toreplayer = 0;
+            torematch = 0;
+        } else {
+            System.out.println("Schade, du hast verloren. Aber beim nächsten Mal läuft es bestimmt besser.");
+            toreplayer = 0;
+            torematch = 0;
+        }
+    }
 
+    void setKickerraum() {
     }
 }
